d2769b43703046f3cc6b225e0ec5bca081080311
jhlive <kingblak_katanga@hotmail.com>
2013-01-26 16:28:43 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vdhackaton::Application.config.secret_token = '4a61152830f1204db06ebf1f2536a643b543bdf08f7c2bb07fff4b62aa3102f409abf7e7c447afeb6f9075065b0a77f229996fc7c0524b2a787036876417f1a1'
