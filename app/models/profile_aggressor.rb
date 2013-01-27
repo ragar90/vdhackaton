50c9ff417363afad811f22ffde9029bd3615a544
Pablo Marti <pablo.marti89@gmail.com>
2013-01-27 12:41:55 -0600

    Edicion de relaciones y nested models para agresores

--- app/models/profile_aggressor.rb
+++ app/models/profile_aggressor.rb
@@ -3,7 +3,7 @@ class ProfileAggressor < ActiveRecord::Base
   belongs_to :parentage
   belongs_to :physical_condition
   belongs_to :weapon_kind
-  has_many :violence_roles
+  has_one :violence_roles
 
   attr_accessible :aftermath_war, :prior_criminal, :sexual_assault_antecedent, :violence_witness
 
