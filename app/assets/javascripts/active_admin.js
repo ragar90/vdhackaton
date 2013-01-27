0d557be812083bfdf5a65e51c4f621b350cf5d93
Pablo Marti <pablo.marti89@gmail.com>
2013-01-27 03:04:09 -0600

    Test sobre active_admin.js 2

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -13,15 +13,6 @@ $(function(){
 			$(this).next().css('display', 'block');
 			$(this).next().next().css('display', 'block');
 		}
-
-	    var div_id = $(this).nextAll("select").attr("id");
-	    var category = $(this).attr("value");
-	    $.ajax({
-	        url: "/day_meals/update_meals_ajax",
-	        type: "GET",
-	        data: {"category_id" : category, id_div : div_id},
-	        dataType: "script"
-	    });*/
 	});
 
 	console.log("Hello hello")
